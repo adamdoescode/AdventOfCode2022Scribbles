adaf53c93725ac584d88e6b37ba94adbc06cf8b8
Adam Graham <adam@Adams-MacBook-Pro.local>
2022-12-04 20:25:08 +0800

    Solution for day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -9,11 +9,16 @@ we would need to expand out each set to do this I think.
 
 
 class ContainedAssignmentPairs():
+    def printPairs(self, pairs):
+        print(
+            pairs
+        )
+    
     def findPairs(self, assignedPairs: str) -> List:
         indicatedPairs = [] #list to hold pairs that meet our conditions
         #find all the pairs where one is a superset of the other
         #each row is a pair
-        for pair in assignedPairs.strip().split('\n'):
+        for pair in assignedPairs:
             assignment1, assignment2 = pair.split(',')
             #each start and stop is seperated by a "-"
             #we want them to be integers so we use a tuple comprehension to assign them as int
@@ -30,9 +35,43 @@ class ContainedAssignmentPairs():
                 pass
         return indicatedPairs
 
-    def sumPairs(self, assignedPairs: str) -> int:
-        indicatedPairs = self.findPairs(assignedPairs)
+    def sumPairs(self, indicatedPairs: List) -> int:
+        '''this works for both parts'''
         return len(indicatedPairs)
+    
+    def processRawInput(self, rawInput: str) -> List:
+        return [pair for pair in rawInput.strip().split('\n')]
+
+    def itemGenerator(self, start,stop) -> str:
+        return [x for x in range(start,stop+1)]
+
+    def findAnyPairs(self, assignedPairs: str) -> List:
+        indicatedPairs = []
+        for pair in assignedPairs:
+            assignment1, assignment2 = pair.split(',')
+            #each start and stop is seperated by a "-"
+            #we want them to be integers so we use a tuple comprehension to assign them as int
+            assignment1Start, assignment1End = (int(number) for number in assignment1.split('-'))
+            assignment2Start, assignment2End = (int(number) for number in assignment2.split('-'))
+            
+            #brute force it
+            assignment1Items = self.itemGenerator(assignment1Start, assignment1End)
+            assignment2Items = self.itemGenerator(assignment2Start, assignment2End)
+            for item in assignment1Items:
+                if item in assignment2Items:
+                    indicatedPairs.append((assignment1, assignment2))
+                    break
+        return indicatedPairs
+
+    def calculateAnyOverlap(self, input: str, part1 = True) -> List:
+        PairList = self.processRawInput(input)
+        if part1:
+            indicatedPairs = self.findPairs(PairList)
+            return self.sumPairs(indicatedPairs)
+        else:
+            #part 2
+            indicatedPairs = self.findAnyPairs(PairList)
+            return self.sumPairs(indicatedPairs)
 
 
 exampleInput = '''2-4,6-8
@@ -42,8 +81,15 @@ exampleInput = '''2-4,6-8
 6-6,4-6
 2-6,4-8'''
 
-ContainedAssignmentPairs().sumPairs(exampleInput)
+ContainedAssignmentPairs().calculateAnyOverlap(exampleInput, part1 = False)
 
 # %%
 with open('input.txt') as f:
     input = f.read()
+
+ContainedAssignmentPairs().calculateAnyOverlap(input, part1 = False)
+
+# %%
+ContainedAssignmentPairs().itemGenerator(6,8)
+
+# %%
