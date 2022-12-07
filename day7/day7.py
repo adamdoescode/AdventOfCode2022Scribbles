f1ba2e4a3c69d23cbae60e717e363df749db75f8
Adam Graham <adam@Adams-MacBook-Pro.local>
2022-12-07 20:53:58 +0800

    Unsovled day7 additions

--- day7/day7.py
+++ day7/day7.py
@@ -22,7 +22,6 @@ class file():
     def printAttrs(self):
         '''Just a way to print the attributes if needed'''
         print(
-            'File:',
             f'Name:\t{self.name}',
             f'size:\t{self.size}',
             f'dir:\t{self.dir}',
@@ -37,50 +36,89 @@ def createNewFile(line, DirName):
                 dir=DirName
             )
 
-dirs: List[str] = []
-#list of files of class file
-files: List[file] = []
+def getFiles(input: str):
+    '''
+    Extract file names, sizes, and which directory they are in
+    '''
 
-#loop through each line in dir structure print out
-currentDirName: str = None
-currentDirDepth: int = 0
-for line in exampleInput.split('\n'):
-    #check if a directory
-    if '(dir)' in line:
-        #get dir name
-        dirName = line.split('-')[-1].split(' (d')[0].strip()
-        dirs.append(dirName)
-        #set current Dir Name to new name
-        currentDirName = dirName
-        currentDirDepth = len(line.split('-')[0])
-    #check if a file
-    elif 'file' in line:
-        #need to detect indentation to use to detect what dir we are in
-        currentLineDepth = len(line.split('-')[0])
-        if currentLineDepth == currentDirDepth:
-            #then the currentDirName remains the same
-            #create a new file which we add to the files list
-            newFile=createNewFile(line, currentDirName)
-            files.append(newFile)
-        elif currentLineDepth < currentDirDepth:
-            #when it is not equal we need to go backwards until we get the right dirname
-            #we can find the higher level dir by dividing number of indents by 2
-            dirsIndex = int(len(line.split('-')[0])/2)-1
-            currentDirName = dirs[dirsIndex]
-            newFile=createNewFile(line, currentDirName)
-            files.append(newFile)
-        elif currentLineDepth > currentDirDepth:
-            #sometimes jump down a directory without it being a dir
-            pass
+    dirs: List[str] = []
+    #list of files of class file
+    files: List[file] = []
 
-    else:
-        print('woah another possibility! See the output below')
-        print(line)
-        break
-        
-[x.printAttrs() for x in files]
+    #loop through each line in dir structure print out
+    currentDirName: str = None
+    currentDirDepth: int = 0
+    #DirFileSize holds the result we care about
+    #we have to initialise the keys in DirFileSize first:
+    #iterate through the file structure line by line
+    for line in input.split('\n'):
+        #check if line is a directory
+        if '(dir)' in line:
+            #get dir name
+            dirName = line.split('-')[-1].split(' (d')[0].strip()
+            dirs.append(dirName)
+            #set current Dir Name to new name
+            currentDirName = dirName
+            currentDirDepth = len(line.split('-')[0])
+        #check if a file
+        elif 'file' in line:
+            #need to detect indentation to use to detect what dir we are in
+            currentLineDepth = len(line.split('-')[0])
+            if currentLineDepth == currentDirDepth:
+                #then the currentDirName remains the same
+                #create a new file which we add to the files list
+                newFile=createNewFile(line, currentDirName)
+                files.append(newFile)
+            elif currentLineDepth < currentDirDepth:
+                #when it is not equal we need to go backwards until we get the right dirname
+                #we can find the higher level dir by dividing number of indents by 2
+                dirsIndex = int(len(line.split('-')[0])/2)-1
+                currentDirName = dirs[dirsIndex]
+                newFile=createNewFile(line, currentDirName)
+                files.append(newFile)
+            elif currentLineDepth > currentDirDepth:
+                #this means the file is the first in the current Dir
+                newFile=createNewFile(line, currentDirName)
+                files.append(newFile)
+                currentDirDepth = currentLineDepth
+
+        else:
+            print('woah another possibility! See the output below')
+            print(line)
+            break
+            
+    [x.printAttrs() for x in files]
+
+getFiles(exampleInput)
 
 # %%
+'''
+Try to use `anytree` to implement tree structure I can use to determine structure for file sizes
+'''
+
+from anytree import Node, RenderTree
+
+root = Node("root")
+a = Node("a", parent=root)
+e = Node("e", parent=a)
+d = Node("d", parent=root)
+print(RenderTree(root))
+
+def getDirTree(input):
+    '''
+    Extract the Directory tree structure using anytree to store this information
+    '''
+    #a list to hold our directories
+    #root directory is always the bottom directory
+    dirTree = []
+
+    for line in input.split('\n'):
+        if '(dir)' in line:
+            #get dir name
+            dirName = line.split('-')[-1].split(' (d')[0].strip()
+            
+            #set current Dir Name to new name
+            currentDirName = dirName
 
 
 # %%
